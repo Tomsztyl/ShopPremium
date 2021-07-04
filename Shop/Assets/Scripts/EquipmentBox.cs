addc0c7c771da40dda767063ef5e0e056589db0b
Tomsztyl <tomek.jelito@gmail.com>
2021-07-04 15:54:50 +0200

    Update EquipmentBox.cs
    -Adding a description
    -Adds items to your internal inventory
    -Adds items to your arsenal's
    -Adds dictionaires items

--- Shop/Assets/Scripts/EquipmentBox.cs
+++ Shop/Assets/Scripts/EquipmentBox.cs
@@ -1,63 +1,106 @@
 using RotaryHeart.Lib.SerializableDictionary;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 
-public class EquipmentBox : ArsenalBox
+public class EquipmentBox : MonoBehaviour
 {
+    [Header("Inventory properties")]
+    [Tooltip("Inventory dictionary")]
+    [SerializeField] private Equipment _equipment;
+    [Tooltip("Arsenal box where the item will be displayed by the script")]
+    [SerializeField] private ArsenalBox _arsenalShopDisplay;
 
-    public void AddItemToEquipment(ShopItems shopItems,float countItem)
+    #region Adding and managing items in dictionaries
+    /// <summary>
+    /// The method adds items to the ArselanBox script where it will be displayed in the item frames
+    /// </summary>
+    public void AddItemToArsenalEquipment()
+    {
+        foreach (var item in _equipment)
+        {
+            AddItemToArsenal(item.Value._itemEquipment, _arsenalShopDisplay.GetEquipmentArsenal(), item.Value._countItemEquipment);
+        }
+    }
+    /// <summary>
+    /// The method adds items to your internal inventory
+    /// </summary>
+    /// <param name="shopItems">An item to be added to your internal inventory</param>
+    /// <param name="_shopEqupment">Internal inventory</param>
+    /// <param name="countItem">Amount of an item added to the internal inventory</param>
+    public void AddItemToEquipment(ShopItems shopItems,Equipment _shopEqupment , float countItem)
     {
         EquipmentItem equipmentItem = new EquipmentItem();
-        equipmentItem.EquipmentAdd(shopItems, countItem);
-        _equipment.Add(IncrementCountIndex(_equipment), equipmentItem);
-    }
-    public int IncrementCountIndex(SerializableDictionaryBase<int, EquipmentItem> someDictionary)
-    {
-        int last = someDictionary.Keys.Last();
-        int increment = last++;
-        if (someDictionary.ContainsKey(increment))
+        ItemAssistant itemAssistant = new ItemAssistant();
+
+        if (_shopEqupment != null)
         {
-            //is contains key [true]
-            return -1;
-        }
-        else
+            if (!itemAssistant.IsObjectInEquipment(_shopEqupment, shopItems))
+            {
+                equipmentItem.EquipmentAdd(shopItems, countItem);
+                _shopEqupment.Add(itemAssistant.IncrementCountIndex(_shopEqupment), equipmentItem);
+            }
+            else
+            {        
+                _shopEqupment[itemAssistant.ObjectInEquipmentIndex(_shopEqupment, shopItems)]._countItemEquipment += countItem;
+            }
+        }  
+    }
+    /// <summary>
+    /// The method adds items to your arsenal's inventory where it will be displayed in the item frame
+    /// </summary>
+    /// <param name="shopItems">Item to be added to the arsenal</param>
+    /// <param name="_shopEqupment">Arsenal where the item will be added</param>
+    /// <param name="countItem">The amount of item that will be added to the arsenal</param>
+    private void AddItemToArsenal(ShopItems shopItems,Equipment _shopEqupment , float countItem)
+    {
+        EquipmentItem equipmentItem = new EquipmentItem();
+        ItemAssistant itemAssistant = new ItemAssistant();
+
+        if (_shopEqupment != null)
         {
-            //no constains key [false]
-            return increment;
-        }
+            if (!itemAssistant.IsObjectInEquipment(_shopEqupment, shopItems))
+            {
+                equipmentItem.EquipmentAdd(shopItems, countItem);
+                _shopEqupment.Add(itemAssistant.IncrementCountIndex(_shopEqupment), equipmentItem);
+            }
+            else
+            {        
+                _shopEqupment[itemAssistant.ObjectInEquipmentIndex(_shopEqupment, shopItems)]._countItemEquipment = countItem;
+            }
+        }  
     }
+    #endregion
 
-    #region Equipment Dictionary
-    [SerializeField]
-    public Equipment _equipment;
-
-    [System.Serializable]
-    public class Equipment : SerializableDictionaryBase<int, EquipmentItem> { }
-
-    [System.Serializable]
-    public class EquipmentItem
+    #region Get Properties
+    public Equipment GetEquipment()
     {
-
-        public ShopItems _itemEquipment;
-        public float _countItemEquipment;
-
-        public void EquipmentAdd (ShopItems _itemEquipment, float _countItemEquipment)
-        {
-            this._itemEquipment= _itemEquipment;
-            this._countItemEquipment = _countItemEquipment;
-        }
+        return _equipment;
     }
-    private EquipmentItem GetValueItem(int _itemKindObjectIndex)
-    {
-        return _equipment.FirstOrDefault(x => x.Key == _itemKindObjectIndex).Value;
-    }
-    private EquipmentItem GetItem(int _itemKindObjectIndex)
-    {
-        return _equipment.FirstOrDefault(x => x.Key == _itemKindObjectIndex).Value;
-    }
-
     #endregion
 }
+
+#region A universal dictionary that holds items
+[System.Serializable]
+public class Equipment : SerializableDictionaryBase<int, EquipmentItem> { }
+
+/// <summary>
+/// The class that stores the properties of the item (Dictionaries)
+/// </summary>
+[System.Serializable]
+public class EquipmentItem
+{
+    public ShopItems _itemEquipment;                //scriptable object where is holded properties item
+    public float _countItemEquipment;               //amount of item stored
+
+    /// <summary>
+    /// The method adds the properties of the item to the dictionary
+    /// </summary>
+    /// <param name="_itemEquipment">Added item</param>
+    /// <param name="_countItemEquipment">Item quantity</param>
+    public void EquipmentAdd(ShopItems _itemEquipment, float _countItemEquipment)
+    {
+        this._itemEquipment = _itemEquipment;
+        this._countItemEquipment = _countItemEquipment;
+    }
+}
+#endregion
